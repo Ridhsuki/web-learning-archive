ac08387ddf2599cdb95a4970541bb2f176935f1f
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-02-13 23:22:31 +0700

    feat (add loginpage n old portfolio)

---
+++ loginPage/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+      'tokped': '#03ac0e',
+      'facebook': '#0866ff'
+    }
+  },
+  },
+  plugins: [],
+}
